12ab955b6def0f0ecbecabe71e5d6b6ca430bac1
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-10-02 13:50:16 +0200

    Set php-cs-fixer dir

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-$finder = PhpCsFixer\Finder::create();
+$finder = PhpCsFixer\Finder::create()->in(__DIR__);
 $config = new Conia\Development\PhpCsFixer\Config();
 
 return $config->setFinder($finder);
