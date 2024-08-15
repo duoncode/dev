a64937eccf57ab156c01a236fda94e5edb0f8b55
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-08-15 19:23:00 +0200

    Use tabs instead of spaces, force \n line endings

--- src/DocsTestRunner.php
+++ src/DocsTestRunner.php
@@ -6,46 +6,46 @@ namespace VacantPlanet\Development;
 
 class DocsTestRunner
 {
-    protected readonly string $title;
-    private readonly string|bool $dir;
+	protected readonly string $title;
+	private readonly string|bool $dir;
 
-    public function __construct(array $args)
-    {
-        $this->title = 'Documentation snippets test runner';
+	public function __construct(array $args)
+	{
+		$this->title = 'Documentation snippets test runner';
 
-        if (!isset($args[1])) {
-            print("{$this->title}: Missing directory operand" . PHP_EOL);
-            print('Try `docs-test-runner path/to/snippets`' . PHP_EOL);
-            exit(1);
-        }
+		if (!isset($args[1])) {
+			print("{$this->title}: Missing directory operand" . PHP_EOL);
+			print('Try `docs-test-runner path/to/snippets`' . PHP_EOL);
+			exit(1);
+		}
 
-        $this->dir = realpath($args[1]);
+		$this->dir = realpath($args[1]);
 
-        if (!$this->dir || !is_dir($this->dir)) {
-            print("{$this->title}: Directory does not exist" . PHP_EOL);
-            print('Directory: ' . $args[1] . PHP_EOL);
-            exit(1);
-        }
-    }
+		if (!$this->dir || !is_dir($this->dir)) {
+			print("{$this->title}: Directory does not exist" . PHP_EOL);
+			print('Directory: ' . $args[1] . PHP_EOL);
+			exit(1);
+		}
+	}
 
-    public function run(): void
-    {
-        $files = glob($this->dir . '/*.php');
-        print($this->title . PHP_EOL . PHP_EOL);
+	public function run(): void
+	{
+		$files = glob($this->dir . '/*.php');
+		print($this->title . PHP_EOL . PHP_EOL);
 
-        foreach ($files as $file) {
-            $file = realpath($file);
-            $line = "\033[33mRun\033[0m: " . basename($file);
-            system('php ' . $file, $value);
+		foreach ($files as $file) {
+			$file = realpath($file);
+			$line = "\033[33mRun\033[0m: " . basename($file);
+			system('php ' . $file, $value);
 
-            if ($value !== 0) {
-                print("{$line} \033[31mERROR\033[0m" . PHP_EOL);
-                exit($value);
-            }
+			if ($value !== 0) {
+				print("{$line} \033[31mERROR\033[0m" . PHP_EOL);
+				exit($value);
+			}
 
-            print("{$line} \033[32mSUCCESS\033[0m" . PHP_EOL);
-        }
+			print("{$line} \033[32mSUCCESS\033[0m" . PHP_EOL);
+		}
 
-        exit(0);
-    }
+		exit(0);
+	}
 }
--- src/PhpCsFixer/Config.php
+++ src/PhpCsFixer/Config.php
@@ -6,38 +6,40 @@ namespace VacantPlanet\Development\PhpCsFixer;
 
 class Config extends \PhpCsFixer\Config
 {
-    public function __construct()
-    {
-        parent::__construct('VacantPlanet');
+	public function __construct()
+	{
+		parent::__construct('VacantPlanet');
 
-        $this->setRiskyAllowed(true);
-    }
+		$this->setRiskyAllowed(true);
+		$this->setIndent("\t");
+		$this->setLineEnding("\n");
+	}
 
-    public function getRules(): array
-    {
-        return [
-            '@PER-CS2.0' => true,
-            'array_syntax' => ['syntax' => 'short'],
-            'blank_line_before_statement' => ['statements' => [
-                'continue', 'declare', 'return', 'throw', 'try', 'if', 'for', 'foreach', 'while', 'do', 'switch',
-            ]],
-            'class_attributes_separation' => ['elements' => ['case' => 'none', 'property' => 'none', 'const' => 'none', 'method' => 'one']],
-            'declare_strict_types' => true,
-            'echo_tag_syntax' => ['format' => 'short', 'shorten_simple_statements_only' => true],
-            'explicit_string_variable' => true,
-            'global_namespace_import' => ['import_classes' => true],
-            'method_argument_space' => true,
-            'method_chaining_indentation' => true,
-            'multiline_whitespace_before_semicolons' => ['strategy' => 'no_multi_line'],
-            'no_alternative_syntax' => ['fix_non_monolithic_code' => false],
-            'no_extra_blank_lines' => ['tokens' => ['extra', 'use']],
-            'no_useless_else' => true,
-            'no_useless_return' => true,
-            'no_superfluous_elseif' => true,
-            'no_whitespace_before_comma_in_array' => true,
-            'ordered_imports' => ['imports_order' => ['class', 'function', 'const'], 'sort_algorithm' => 'alpha'],
-            'semicolon_after_instruction' => false,
-            'types_spaces' => ['space' => 'none', 'space_multiple_catch' => 'single'],
-        ];
-    }
+	public function getRules(): array
+	{
+		return [
+			'@PER-CS2.0' => true,
+			'array_syntax' => ['syntax' => 'short'],
+			'blank_line_before_statement' => ['statements' => [
+				'continue', 'declare', 'return', 'throw', 'try', 'if', 'for', 'foreach', 'while', 'do', 'switch',
+			]],
+			'class_attributes_separation' => ['elements' => ['case' => 'none', 'property' => 'none', 'const' => 'none', 'method' => 'one']],
+			'declare_strict_types' => true,
+			'echo_tag_syntax' => ['format' => 'short', 'shorten_simple_statements_only' => true],
+			'explicit_string_variable' => true,
+			'global_namespace_import' => ['import_classes' => true],
+			'method_argument_space' => true,
+			'method_chaining_indentation' => true,
+			'multiline_whitespace_before_semicolons' => ['strategy' => 'no_multi_line'],
+			'no_alternative_syntax' => ['fix_non_monolithic_code' => false],
+			'no_extra_blank_lines' => ['tokens' => ['extra', 'use']],
+			'no_useless_else' => true,
+			'no_useless_return' => true,
+			'no_superfluous_elseif' => true,
+			'no_whitespace_before_comma_in_array' => true,
+			'ordered_imports' => ['imports_order' => ['class', 'function', 'const'], 'sort_algorithm' => 'alpha'],
+			'semicolon_after_instruction' => false,
+			'types_spaces' => ['space' => 'none', 'space_multiple_catch' => 'single'],
+		];
+	}
 }
